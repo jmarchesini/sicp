d5e625accb87660f97226e9934d3d71af7c27b7a
jmarchesini <john.marchesini@gmail.com>
2021-04-19 19:48:10 -0500

    Add 2.51

---
+++ chapter2/ex2.51.scm
@@ -0,0 +1,27 @@
+(define (below-1 painter1 painter2)
+  (let ((split-point (make-vert 0.0 0.5)))
+    (let ((paint-bottom
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              (make-vect 1.0 0.0)
+                              split-point))
+          (paint-top
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.5)
+                              (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+
+(define (rotate-90 painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+
+(define (below-2 painter1 painter2)
+  (rotate-90 (beside (rotate-270 painter1)
+                     (rotate-270 painter2))))
