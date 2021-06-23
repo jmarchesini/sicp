aa9aa51744fd2c49ea74d1057634c2080fcfe573
jmarchesini <john.marchesini@gmail.com>
2021-06-23 17:46:47 -0500

    Add output to 3.12

--- chapter3/ex3.12.scm
+++ chapter3/ex3.12.scm
@@ -6,3 +6,29 @@
   (if (null? (cdr x))
       x
       (last-pair (cdr x))))
+
+
+;; 1 ]=> (define x (list 'a 'b))
+;; ;Value: x
+
+;; 1 ]=> (define y (list 'c 'd))
+;; ;Value: y
+
+;; 1 ]=> (define z (append x y))
+;; ;Value: z
+
+;; 1 ]=> z
+;; ;Value: (a b c d)
+
+;; 1 ]=> (cdr x)
+;; ;Value: (b)
+
+;; 1 ]=> (define w (append! x y))
+;; ;Value: w
+
+;; 1 ]=> w
+;; ;Value: (a b c d)
+
+;; 1 ]=> (cdr x)
+;; ;Value: (b c d)
+
