8dd89811fba353cf2bdbadbd97dffcf45422beb0
jmarchesini <john.marchesini@gmail.com>
2021-07-11 12:33:27 -0500

    Add helpers for 3.54

--- chapter3/stream.scm
+++ chapter3/stream.scm
@@ -14,3 +14,13 @@
 
 (define (add-streams s1 s2)
   (stream-map + s1 s2))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
+
+(define ones
+  (cons-stream 1 ones))
+
+(define integers
+  (cons-stream 1 (add-streams ones integers)))
+                              
