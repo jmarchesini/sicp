c47f98bcf514d416c1366840b46ff48e668a2935
jmarchesini <john.marchesini@gmail.com>
2021-10-10 21:23:01 -0500

    Add ex4.1

---
+++ chapter4/ex4.1.scm
@@ -0,0 +1,21 @@
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values 
+             (rest-operands exps) 
+             env))))
+
+(define (list-of-values-l2r exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((left (eval (first-operand exps) env))
+            (right (list-of-values-l2r (rest-operands exps) env)))
+        (cons left right))))
+
+(define (list-of-values-r2l exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((right (list-of-values-r2l (rest-operands exps) env))
+            (left (eval (first-operand exps) env)))
+        (cons left right))))
