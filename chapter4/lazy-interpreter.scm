efebec6af7eb75ea4eb7b62bdb865df0febbf6ca
jmarchesini <john.marchesini@gmail.com>
2021-10-25 18:29:50 -0500

    Add lazy interpreter

--- chapter4/lazy-interpreter.scm
+++ chapter4/lazy-interpreter.scm
@@ -1,4 +1,4 @@
-;; Interpreter given in Section 4.1
+;; Interpreter given in Section 4.2
 (define apply-in-underlying-scheme apply)
 
 (define (eval exp env)
@@ -17,34 +17,46 @@
                        env))
         ((cond? exp) (eval (cond->if exp) env))
         ((application? exp)
-         (apply
-          (eval (operator exp) env)
-          (list-of-values (operands exp) env)))
+         (apply (actual-value (operator exp) env)
+                (operands exp)
+                env))
         (else
          (error "Unknown expression type: EVAL" exp))))
 
-(define (apply procedure arguments)
+(define (apply procedure arguments env)
   (cond ((primitive-procedure? procedure)
-         (apply-primitive-procedure procedure arguments))
+         (apply-primitive-procedure
+          procedure
+          (list-of-arg-values arguments env)))
         ((compound-procedure? procedure)
          (eval-sequence
           (procedure-body procedure)
           (extend-environment (procedure-parameters procedure)
-                              arguments
+                              (list-of-delayed-args arguments env)
                               (procedure-environment procedure))))
         (else
          (error "Unknown procedure type: APPLY" procedure))))
 
+;; Actual value - forces thunks
+(define (actual-value exp env)
+  (force-it (eval exp env)))
+
 ;; Procedure arguments
-(define (list-of-values exp env)
-  (if (no-operands? exp)
+(define (list-of-arg-values exps env)
+  (if (no-operands? exps)
       '()
-      (cons (eval (first-operand exp) env)
-            (list-of-values (rest-operands exp) env))))
+      (cons (actual-value (first-operand exps) env)
+            (list-of-arg-values (rest-operands exps) env))))
+
+(define (list-of-delayed-args exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (delay-it (first-operand exps) env)
+            (list-of-delayed-args (rest-operands exps) env))))
 
 ;; Conditionals
 (define (eval-if exp env)
-  (if (true? (eval (if-predicate exp) env))
+  (if (true? (actual-value (if-predicate exp) env))
       (eval (if-consequent exp) env)
       (eval (if-alternative exp) env)))
 
@@ -352,6 +364,43 @@
   (apply-in-underlying-scheme
    (primitive-implementation proc) args))
 
+;; Thunks
+(define (force-it obj)
+  (if (thunk? obj)
+      (actual-value (thunk-exp obj) (thunk-env obj))
+      obj))
+
+(define (delay-it exp env)
+  (list 'thunk exp env))
+
+(define (thunk? obj)
+  (tagged-list? obj 'thunk))
+
+(define (thunk-exp thunk)
+  (cadr thunk))
+
+(define (thunk-env thunk)
+  (caddr thunk))
+
+;; Memoized thunks
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+
+(define (thunk-value evaluated-thunk)
+  (cadr evaluated-thunk))
+
+(define (force-it obj)
+  (cond ((thunk? obj)
+         (let ((result (actual-value (thunk-exp obj)
+                                     (thunk-env obj))))
+           (set-car! obj 'evaluated-thunk)
+           (set-car! (cdr obj) result)
+           (set-cdr! (cdr obj) '())
+           result))
+        ((evaluated-thunk? obj)
+         (thunk-value obj))
+        (else obj)))
+
 ;; Driver
 (define (setup-environment)
   (let ((initial-env
@@ -364,14 +413,14 @@
 
 (define the-global-environment (setup-environment))
 
-(define input-prompt ";;; M-Eval input:")
+(define input-prompt ";;; L-Eval input:")
 
-(define output-prompt ";;; M-Eval value:")
+(define output-prompt ";;; L-Eval value:")
 
 (define (driver-loop)
   (prompt-for-input input-prompt)
   (let ((input (read)))
-    (let ((output (eval input the-global-environment)))
+    (let ((output (actual-value input the-global-environment)))
       (announce-output output-prompt)
       (user-print output)))
   (driver-loop))
@@ -391,3 +440,4 @@
       (display object)))
 
 (define the-global-environment (setup-environment))
+(driver-loop)
