90105dc469271c0fa7a96ccf9e8d45a9516918ba
jmarchesini <john.marchesini@gmail.com>
2021-11-13 19:11:14 -0600

    Add ex4.55

---
+++ chapter4/ex4.55.scm
@@ -0,0 +1,8 @@
+;; all people supervised by Ben B.
+(supervisor ?person (Bitdiddle Ben))
+
+;; the names and jobs of all people in the accounting division
+(job ?person (accounting . ?title))
+
+;; the names and addresses of all people who live in Slumerville
+(address ?person (Slumerville . ?address))
